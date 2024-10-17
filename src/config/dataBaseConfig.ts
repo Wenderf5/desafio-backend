aa8b6b2eaf71b2fa418e9febd59d1cb6c14e2aef
wenderf5 <wenderfabianoalves@gmail.com>
2024-10-17 15:15:59 -0300

    feat: sign-up user

--- src/config/dataBaseConfig.ts
+++ src/config/dataBaseConfig.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const dataBaseConfig: TypeOrmModuleOptions = {
     type: 'mysql',
-    host: 'localhost',
+    host: 'host',
     port: 3306,
-    username: 'root',
-    password: 'wfas54321',
-    database: 'desafio_back_end',
+    username: 'user',
+    password: 'password',
+    database: 'data-base-name',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: false,
 }
